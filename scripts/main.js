f5fa82deb576ed24274080eb701ad4afb6682285
Pranav Patel <pranavpatelgoa@gmail.com>
2021-09-28 01:24:11 +0530

    feat: scripting for isTriangle?

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+const angles = document.querySelectorAll('.angle-input');
+const isTriangleBtn = document.querySelector('#is-triangle-btn');
+const isTriangleOutput = document.querySelector('#is-triangle-output');
+
+function calculateSumOfAngles() {
+	const sumOfAngles =
+		Number(angles[0].value) + Number(angles[1].value) + Number(angles[2].value);
+	console.log(sumOfAngles);
+	return sumOfAngles;
+}
+
+function isTriangle() {
+	if (calculateSumOfAngles() === 180) {
+		isTriangleOutput.innerText = 'Yes!! These angles form a triangle!';
+	} else {
+		isTriangleOutput.innerText = 'Oh no, you can not form a triangle with these angles!';
+	}
+}
+isTriangleBtn.addEventListener('click', isTriangle);
